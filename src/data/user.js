b242368b3e2612a422ca8533d5e37138defe745c
daeyeonlee <angellee98@naver.com>
2025-11-26 10:48:38 +0900

    SSAFY 내용 수정

--- src/data/user.js
+++ src/data/user.js
@@ -1,82 +1,3 @@
-// const HomepageInfo = {
-// 	main: {
-// 		title: "Daeyeon Lee - Portfolio",
-// 		name: "Daeyeon Lee",
-// 		email: "angellee98@naver.com",
-// 		logo: "../logo.png",
-// 		jobTitle: "Frontend Developer",
-// 	},
-
-// 	socials: {
-// 		github: "https://github.com/daeyeon-lee",
-// 		instagram: "https://instagram.com/",
-// 		blog: "https://velog.io/@dylee98/",
-// 	},
-
-// 	profiles: {
-// 		title: "이대연",
-// 		jobTitle: "Frontend Developer",
-// 		description: [
-// 			"사용자 중심의 웹 애플리케이션 개발에 집중하며,",
-// 			"성능 최적화와 접근성을 고려한 프론트엔드 개발을 통해 사용자 경험을 향상시키는 개발자입니다.",
-// 		],
-// 	},
-
-// 	stacks: [
-// 		{
-// 			name: "Languages",
-// 			items: ["Python", "JavaScript", "TypeScript"],
-// 		},
-// 		{
-// 			name: "Frontend",
-// 			items: [
-// 				"React",
-// 				"React Native",
-// 				"Next.js",
-// 				"Vue.js",
-// 				"Tailwind CSS",
-// 				"HTML",
-// 				"CSS",
-// 			],
-// 		},
-// 		{
-// 			name: "Database",
-// 			items: ["MySQL"],
-// 		},
-// 		{
-// 			name: "Tools",
-// 			items: ["Git", "GitHub", "GitLab", "Jira", "Figma"],
-// 		},
-// 	],
-
-// 	skills: {
-// 		frontend: [
-// 			"4개의 팀 프로젝트 개발 경험",
-// 			"Swagger 기반 REST API 연동 개발 경험",
-// 			"Vue 3 Composition API 기반 UI 컴포넌트 설계 ",
-// 			"React + Zustand 기반 공통 UI 컴포넌트와 디자인 시스템을 구축",
-// 			"Next.js 기반 공통 UI 컴포넌트와 디자인 시스템을 구축",
-// 			"React Native + React Webview 기반의 하이브리드 앱 개발 경험",
-// 			"FCM을 이용한 알림 기능 개발 경험",
-// 			"반응형 디자인을 고려한 마크업 UI 개발",
-// 		],
-// 	},
-
-// 	about: {
-// 		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
-// 		description:
-// 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
-// 	},
-
-// 	articles: {
-// 		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-// 		description:
-// 			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-// 	},
-// };
-
-// export default HomepageInfo;
-
 const INFO = {
 	main: {
 		title: "이대연, Portfolio",
@@ -192,14 +113,14 @@ const INFO = {
 	activities: [
 		{
 			period: "2025.01. ~ 2025.12.",
-			title: "삼성 청년 SW 아카데미",
-			subtitle: "삼성 청년 SW 아카데미 13기 활동",
+			title: "삼성 청년 SW-AI 아카데미",
+			subtitle: "삼성 청년 SW-AI 아카데미 13기 활동",
 			description: [
-				"삼성, 고용노동부 주관 SW 역량 향상 교육 프로그램",
+				"삼성, 고용노동부 주관 SW-AI 역량 향상 교육 프로그램",
 				"Python, 알고리즘, Vue.js, HTML/CSS/JavaScript/TypeScript, React, Django기반 웹 서비스 구축 및 DB 설계에 대해서 학습했습니다.",
 				"팀 프로젝트를 통한 협업 경험",
 				"1학기 프로젝트 우수상, 2학기 프로젝트 우수상 3회 수상",
-				"우수 교육생으로 3회 선정",
+				"우수 교육생 3회 선정",
 			],
 		},
 
